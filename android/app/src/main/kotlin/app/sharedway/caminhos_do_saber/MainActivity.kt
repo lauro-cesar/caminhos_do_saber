008b856202ff3e95f41dcbb8c44093bf9818abce
Lauro C Oliveira <lauro@hostcert.com.br>
2020-04-05 17:21:16 -0300

    First Commit

---
+++ android/app/src/main/kotlin/app/sharedway/caminhos_do_saber/MainActivity.kt
@@ -0,0 +1,12 @@
+package app.sharedway.caminhos_do_saber
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
